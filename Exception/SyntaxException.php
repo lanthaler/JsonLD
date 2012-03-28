3ea167762a4eca1b7c5bdb84b7bebe6b8fa24ac7
Markus Lanthaler <mark_lanthaler@gmx.net>
2012-03-28 19:48:24 +0800

    Implement value and IRI expansion
    Implement value and IRI expansion (handling of relative IRIs is still missing). Improved expand() but still not complete.

---
+++ Exception/SyntaxException.php
@@ -0,0 +1,113 @@
+<?php
+
+/*
+ * (c) Markus Lanthaler <mail@markus-lanthaler.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ML\JsonLD\Exception\Exception;
+
+/**
+ * Exception class thrown when a JSON-LD syntax error is detected.
+ *
+ * @author Markus Lanthaler <mail@markus-lanthaler.com>
+ *
+ * @api
+ */
+class SyntaxException extends \RuntimeException
+{
+    private $parsedFile;
+    private $snippet;
+    private $rawMessage;
+
+    /**
+     * Constructor.
+     *
+     * @param string    $message    The error message
+     * @param string    $parsedFile The file name where the error occurred
+     * @param Exception $previous   The previous exception
+     */
+    public function __construct($message, $snippet = null, $parsedFile = null, Exception $previous = null)
+    {
+        $this->parsedFile = $parsedFile;
+        $this->snippet = $snippet;
+        $this->rawMessage = $message;
+
+        $this->updateMessage();
+
+        parent::__construct($this->message, 0, $previous);
+    }
+
+    /**
+     * Gets the snippet of code near the error.
+     *
+     * @return string The snippet of code
+     */
+    public function getSnippet()
+    {
+        return $this->snippet;
+    }
+
+    /**
+     * Sets the snippet of code near the error.
+     *
+     * @param string $snippet The code snippet
+     */
+    public function setSnippet($snippet)
+    {
+        $this->snippet = $snippet;
+
+        $this->updateMessage();
+    }
+
+    /**
+     * Gets the filename where the error occurred.
+     *
+     * This method returns null if a string is parsed.
+     *
+     * @return string The filename
+     */
+    public function getParsedFile()
+    {
+        return $this->parsedFile;
+    }
+
+    /**
+     * Sets the filename where the error occurred.
+     *
+     * @param string $parsedFile The filename
+     */
+    public function setParsedFile($parsedFile)
+    {
+        $this->parsedFile = $parsedFile;
+
+        $this->updateMessage();
+    }
+
+    /**
+     * Updates the exception message by including the file name if available.
+     */
+    private function updateMessage()
+    {
+        $this->message = $this->rawMessage;
+
+        $dot = false;
+        if ('.' === substr($this->message, -1)) {
+            $this->message = substr($this->message, 0, -1);
+            $dot = true;
+        }
+
+        if (null !== $this->parsedFile) {
+            $this->message .= sprintf(' in %s', json_encode($this->parsedFile));
+        }
+
+        if ($this->snippet) {
+            $this->message .= sprintf(' (near "%s")', $this->snippet);
+
+        if ($dot) {
+            $this->message .= '.';
+        }
+    }
+}
--- Processor.php
+++ Processor.php
@@ -10,6 +10,7 @@
 namespace ML\JsonLD;
 
 use ML\JsonLD\Exception\ParseException;
+use ML\JsonLD\Exception\SyntaxException;
 use ML\JsonLD\Exception\ProcessException;
 
 /**
@@ -87,7 +88,7 @@ class Processor
     /**
      * Expands a JSON-LD document.
      *
-     * @param mixed  $jsonld     An already parsed JSON-LD document
+     * @param mixed  $element    A JSON-LD elemnt to be expanded
      * @param array  $activectx  The active context
      * @param string $baseiri    The base IRI
      *
@@ -95,43 +96,35 @@ class Processor
      *
      * @throws ParseException If the JSON-LD document is not valid
      */
-    public function expand(&$jsonld, $activectx = array(), $baseiri = '')
+    public function expand(&$element, $activectx = array(), $baseiri = '')
     {
-        if (is_object($jsonld))
+        // TODO Spec: Rename value to element to make it distinguishable
+        // TODO Spec: Expand doesn't need the active property
+        // TODO Spec: Remove local context immediately after processing it
+        // TODO Spec: Define precedence @graph, @value
+        // TODO Spec: Rename key to property
+        // TODO Spec: Indent 2.3 onwards
+        // TODO Spec: 2.2.4) Otherwise, if the key is @id or @type and the value is a string, expand the value according to IRI Expansion. -> @type can be array
+
+        if (is_array($element))
         {
-            if (property_exists($jsonld, '@context'))
+            foreach($element as &$item)
             {
-                $this->updateActiveContext($activectx, $jsonld->{'@context'});
-                unset($jsonld->{'@context'});
+                $item = $this->expand($item, $activectx);
+            }
+        }
+        else if (is_object($element))
+        {
+            if (property_exists($element, '@context'))
+            {
+                $this->updateActiveContext($activectx, $element->{'@context'});
+                unset($element->{'@context'});
             }
 
             // TODO Define precedence
-            if (property_exists($jsonld, '@graph'))
+            if (property_exists($element, '@graph'))
             {
                 // TODO Check for invalid other properties
-            }
-
-            if (property_exists($jsonld, '@type'))
-            {
-                // TODO without @value
-                if (is_string($jsonld->{'@type'}))
-                {
-                    $jsonld->{'@type'} = $this->expandIri($jsonld->{'@type'}, $activectx, $baseiri);
-                }
-                elseif (is_array($jsonld->{'@type'}))
-                {
-                    foreach ($jsonld->{'@type'} as &$iri)
-                    {
-                        // TODO Check if string
-                        $iri = $this->expandIri($iri, $activectx, $baseiri);
-                    }
-                }
-                else
-                {
-                    throw new ParseException(
-                        'Invalid value for @type detected. Expected string or array, got ' .
-                        var_export($jsonld->{'@type'}, true));
-                }
             }
 
             // TODO Check @value (with @type), @language, @list, @list
@@ -139,36 +132,172 @@ class Processor
             // TODO Check @container -> invalid
 
             // Process all other properties
-            foreach ($jsonld as $key => $value)
+            $properties = get_object_vars($element);
+            foreach ($properties as $key => $value)
             {
-                if (key_exists($key, $activectx))
+                if (is_null($value))
                 {
-                    // Remove un-expanded property
-                    if ($key !== $activectx[$key])
+                    unset($element->{$key});
+                    continue;
+                }
+                elseif(is_object($value))
+                {
+                    if (((property_exists($value, '@value')) && (false === isset($value->{'@value'}))) ||
+                        ((property_exists($value, '@list')) && (false === isset($value->{'@list'}))) ||
+                        ((property_exists($value, '@set')) && (false === isset($value->{'@set'}))))
                     {
-                        unset($jsonld->{$key});
+                        unset($element->{$key});
+                        continue;
+                    }
+                    elseif (property_exists($value, '@set') && isset($value->{'@set'}))
+                    {
+                        $value = $value->{'@set'};
+                    }
+                }
+
+                // TODO Add support for keyword aliasing
+                if ('@id' == $key)  // TODO Add support for keyword aliasing
+                {
+                    // TODO without @value
+                    if (is_string($value))
+                    {
+                        $element->{'@id'} = $this->expandIri($value, $activectx, $baseiri);
+                    }
+                    else
+                    {
+                        throw new ParseException(
+                            'Invalid value for @id detected. Expected string or array, got ' .
+                            var_export($element->{'@type'}, true));
+                    }
+                }
+                elseif ('@type' == $key)
+                {
+                    // TODO without @value
+                    if (is_string($element->{'@type'}))
+                    {
+                        $element->{'@type'} = array($element->{'@type'});
                     }
 
-                    // Create expanded property
-                    if (false === property_exists($jsonld, $activectx[$key]['@id']))
+                    if (is_array($element->{'@type'}))
                     {
-                        $jsonld->{$activectx[$key]['@id']} = array();
+                        foreach ($element->{'@type'} as &$iri)
+                        {
+                            // TODO Check if string
+                            $iri = $this->expandIri($iri, $activectx, $baseiri);
+                        }
                     }
+                    else
+                    {
+                        throw new ParseException(
+                            'Invalid value for @type detected. Expected string or array, got ' .
+                            var_export($element->{'@type'}, true));
+                    }
+                }
+                elseif ('@value' == $key)  // TODO Add support for keyword aliasing
+                {
+                    // nothing to do, already expanded
+                    continue;
+                }
+                else // TODO Check if keyword, if not, continue below
+                {
+                    // TODO Handle plain-old JSON properties
+                    $expandedKey = $this->expandIri($key, $activectx, $baseiri);
+
+                    // Remove un-expanded property
+                    if ($key !== $expandedKey)
+                    {
+                        unset($element->{$key});
+                    }
+
+                    // Create expanded property and make sure it's an array
+                    if (false === property_exists($element, $expandedKey))
+                    {
+                        $element->{$expandedKey} = array();
+                    }
+                    elseif (false === is_array($element->{$expandedKey}))
+                    {
+                        $element->{$expandedKey} = array($element->{$expandedKey});
+                    }
+
+                    if (isset($activectx[$key]['@container']) && ('@list' === $activectx[$key]['@container'])) // TODO check not already in @list form
+                    {
+                        // TODO Fix this
+                        $obj = new \stdClass();
+                        $obj->{'@list'} = $value;
+                        $element->{$expandedKey}[] = $obj;
+                    }
+
 
                     // Expand value
-                    $this->expand($value, $activectx);
-                    $jsonld->{$activectx[$key]['@id']}[] = $value;
+                    /*if (is_object($value))
+                    {
+                        // TODO Check precendence
+                        $this->expand($element->{$key}, $activectx, $baseiri);
+                    }
+                    else*/if (is_array($value))
+                    {
+                        foreach ($value as $item)
+                        {
+                            $element->{$expandedKey}[] = $this->expand($item, $key, $activectx, $baseiri);
+                        }
+                    }
+                    else
+                    {
+                        // TODO Expand if coercion/language taggin etc.
+                        $element->{$expandedKey}[] = $this->expandValue($value, $key, $activectx, $baseiri);
+                    }
+                    // else: key is not defined in context, ignore it and subtree
                 }
-                // else: key is not defined in context, ignore it and subtree
             }
         }
-        elseif (is_array($jsonld))
+    }
+
+    /**
+     * Expands a JSON-LD value.
+     *
+     * The value can be of any scalar type (i.e., not an object or array)
+     *
+     * @param mixed  $value      The value to be expanded to an absolute IRI
+     * @param mixed  $activeprty The active property
+     * @param array  $activectx  The active context
+     * @param string $baseiri    The base IRI
+     *
+     * @return StdClass  The expanded value in object form
+     */
+    protected function expandValue($value, $activeprty, $activectx = array(), $baseiri)
+    {
+        if (isset($activectx[$activeprty]))
         {
-            foreach($jsonld as &$item)
+            $activeprty = $activectx[$activeprty];
+
+            if (isset($activeprty['@type']))
             {
-                $item = $this->expand($item, $activectx);
+                // TODO 1) If value is a number and the active property is the target of typed literal coercion to xsd:integer or xsd:double, expand the value into an object with two key-value pairs. The first key-value pair will be @value and the string representation of value as defined in the section Data Round Tripping. The second key-value pair will be @type and the associated coercion datatype expanded according to the IRI Expansion rules.
+
+                if ('@id' == $activeprty['@type'])
+                {
+                    $obj = new \stdClass();
+                    $obj->{'@id'} = $this->expandIri($value, $activectx, $baseiri);
+                    return $obj;
+                }
+                else
+                {
+                    // TODO Add special cases for xsd:double, xsd:integer!?
+                    $obj = new \stdClass();
+                    $obj->{'@value'} = $value;
+                    $obj->{'@type'} = $activeprty['@type'];  // TODO Make sure types are already expanded
+                    return $obj;
+                }
+            }
+            elseif (isset($activeprty['@language']) && is_string($value))  // TODO or global language
+            {
+                $obj = new \stdClass();
+                $obj->{'@value'} = $value;
+                $obj->{'@language'} = $activeprty['@language'];
+                return $obj;
             }
         }
+        return $value;
     }
 
     /**
@@ -180,17 +309,18 @@ class Processor
      *
      * @return StdClass  The expanded value in object form
      */
-    protected function expandIri($value, $activectx = array(), $baseiri)
+    private function expandIri($value, $activectx = array(), $baseiri)
     {
         // TODO Handle relative IRIs
 
-        if (array_key_exists($value, $activectx))
+        if (array_key_exists($value, $activectx) && isset($activectx[$value]['@id']))
         {
-            return $activectx[$value];
+            return $activectx[$value]['@id'];
         }
-        elseif ((false !== ($colon = strpos($value, ':'))) && ($colon < strlen($value) - 1))
+
+        if (false !== ($colon = strpos($value, ':')))
         {
-            if ('://' == substr($value, $colon, 2))
+            if ('://' == substr($value, $colon, 3))
             {
                 // Safety measure to prevent reassigned of, e.g., http://
                 return $value;
@@ -211,6 +341,8 @@ class Processor
             }
         }
 
+        // TODO Handle relative IRIs (add to spec)
+
         // can't expand it, return as is
         return $value;
     }
@@ -223,7 +355,7 @@ class Processor
      * @param array  $activectx  The active context
      * @param array  $path       A path of already processed terms
      *
-     * @throws ProcessException If the JSON-LD document is not valid
+     * @throws SyntaxException If a JSON-LD syntax error is detected
      */
     protected function contextIriExpansion($value, $loclctx, $activectx, $path = array())
     {
@@ -237,8 +369,8 @@ class Processor
 
         if (in_array($prefix, $path))
         {
-            throw new ProcessException('Cycle in context definition detected: ' .
-                                       join(' -> ', $path) . ' -> ' . $path[0]);
+            throw new SyntaxException('Cycle in context definition detected: ' .
+                                      join(' -> ', $path) . ' -> ' . $path[0]);
         }
         else
         {
@@ -260,60 +392,61 @@ class Processor
     }
 
     /**
-     * Updates the active context with a local context.
+     * Processes a local context to update the active context.
      *
      * @param array  $activectx  The active context
      * @param array  $loclctx    The local context
      *
-     * @throws ProcessException If the JSON-LD document is not valid
+     * @throws ProcessException If processing of the JSON-LD document failed
      */
     protected function updateActiveContext(&$activectx, $loclctx)
     {
-      foreach ($loclctx as $key => $value)
-      {
-        if (is_null($value))
+        // TODO Make sure that all @id's are absolute IRIs
+        foreach ($loclctx as $key => $value)
         {
-            unset($activectx[$key]);
-        }
-        elseif (is_string($value))
-        {
-            // either IRI or prefix:suffix
-            $expanded = $this->contextIriExpansion($value, $loclctx, $activectx);
-            $loclctx->{$key} = $expanded;
-            $activectx[$key]['@id'] = $expanded;
-        }
-        elseif (is_object($value))
-        {
-            if (property_exists($value, '@id'))
+            if (is_null($value))
             {
-                if (is_null($value->{'@id'}))
+                unset($activectx[$key]);
+            }
+            elseif (is_string($value))
+            {
+                // either IRI or prefix:suffix
+                $expanded = $this->contextIriExpansion($value, $loclctx, $activectx);
+                $loclctx->{$key} = $expanded;
+                $activectx[$key]['@id'] = $expanded;
+            }
+            elseif (is_object($value))
+            {
+                if (property_exists($value, '@id'))
                 {
-                    unset($activectx[$key]);
+                    if (is_null($value->{'@id'}))
+                    {
+                        unset($activectx[$key]);
+                    }
+                    else
+                    {
+                        $expanded = $this->contextIriExpansion($value->{'@id'}, $loclctx, $activectx);
+                        $loclctx->{$key}->{'@id'} = $expanded;
+                        $activectx[$key]['@id'] = $expanded;
+                    }
                 }
-                else
+
+                if(property_exists($value, '@type'))
                 {
-                    $expanded = $this->contextIriExpansion($value->{'@id'}, $loclctx, $activectx);
-                    $loclctx->{$key}->{'@id'} = $expanded;
-                    $activectx[$key]['@id'] = $expanded;
+                    $expanded = $this->contextIriExpansion($value->{'@type'}, $loclctx, $activectx);
+
+                    if(!is_null($expanded))
+                    {
+                        $loclctx->{$key}->{'@type'} = $expanded;
+                        $activectx[$key]['@type'] = $expanded;
+                    }
                 }
             }
-
-            if(property_exists($value, '@type'))
+            else
             {
-                $expanded = $this->contextIriExpansion($value->{'@type'}, $loclctx, $activectx);
-
-                if(!is_null($expanded))
-                {
-                    $loclctx->{$key}->{'@type'} = $expanded;
-                    $activectx[$key]['@type'] = $expanded;
-                }
+                throw new ProcessException('Found invalid value in context.',
+                                           $key . ': ' . json_encode($value, JSON_UNESCAPED_SLASHES));
             }
         }
-        else
-        {
-            throw new ProcessException('Found invalid value in context.',
-                                       $key . ': ' . json_encode($value, JSON_UNESCAPED_SLASHES));
-        }
-      }
     }
 }
