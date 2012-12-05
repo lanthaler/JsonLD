b98a93f890857036db162a0d2263c918ffc5070f
Markus Lanthaler <mark_lanthaler@gmx.net>
2012-12-05 14:45:54 +0100

    Ignore the order of object members in the test suite

--- Test/JsonLDTestSuiteTest.php
+++ Test/JsonLDTestSuiteTest.php
@@ -56,7 +56,7 @@ class JsonLDTestSuiteTest extends \PHPUnit_Framework_TestCase
         $result = JsonLD::expand($this->basedir . $test->{'input'},
                                  $options);
 
-        $this->assertEquals($expected, $result);
+        $this->assertJsonEquals($expected, $result);
     }
 
     /**
@@ -83,7 +83,7 @@ class JsonLDTestSuiteTest extends \PHPUnit_Framework_TestCase
                                   $this->basedir . $test->{'context'},
                                   $options);
 
-        $this->assertEquals($expected, $result);
+        $this->assertJsonEquals($expected, $result);
     }
 
 
@@ -117,7 +117,7 @@ class JsonLDTestSuiteTest extends \PHPUnit_Framework_TestCase
                                 $this->basedir . $test->{'frame'},
                                 $options);
 
-        $this->assertEquals($expected, $result);
+        $this->assertJsonEquals($expected, $result);
     }
 
 
@@ -188,5 +188,55 @@ class JsonLDTestSuiteTest extends \PHPUnit_Framework_TestCase
     public function fromQuadsProvider()
     {
         return new TestManifestIterator($this->basedir . 'fromRdf-manifest.jsonld');
+    }
+
+
+    /**
+     * Asserts that two JSON structures are equal.
+     *
+     * @param  object|array $expected
+     * @param  object|array $actual
+     * @param  string $message
+     */
+    public static function assertJsonEquals($expected, $actual, $message = '')
+    {
+        $expected = self::normalizeJson($expected);
+        $actual = self::normalizeJson($actual);
+
+        self::assertEquals($expected, $actual, $message);
+    }
+
+
+    /**
+     * Brings the keys of objects to a deterministic order to enable
+     * comparison of JSON structures
+     *
+     * @param mixed $element The element to normalize.
+     *
+     * @return mixed The same data with all object keys ordered in a
+     *               deterministic way.
+     */
+    private static function normalizeJson($element)
+    {
+        if (is_array($element))
+        {
+            foreach ($element as &$item)
+            {
+                $item = self::normalizeJson($item);
+            }
+        }
+        elseif (is_object($element))
+        {
+            $element = (array) $element;
+            ksort($element);
+            $element = (object) $element;
+
+            foreach ($element as $key => &$item)
+            {
+                $item = self::normalizeJson($item);
+            }
+        }
+
+        return $element;
     }
 }
