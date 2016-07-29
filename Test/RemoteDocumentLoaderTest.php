738991a2632db365479a5a0c9ffe3967baa29052
zema <zlobzn@gmail.com>
2016-07-29 11:52:17 +0300

    injection of RemoteDocumentLoader

--- Test/RemoteDocumentLoaderTest.php
+++ Test/RemoteDocumentLoaderTest.php
@@ -10,17 +10,23 @@ class RemoteDocumentLoaderTest extends \PHPUnit_Framework_TestCase
 {
     public function testInjection()
     {
+        // get default loader
         $defaultLoader = JsonLD::getRemoteDocumentLoader();
         $this->assertInstanceOf('ML\\JsonLD\\RemoteDocumentLoader', $defaultLoader);
         $this->assertInstanceOf('ML\\JsonLD\\FileGetContentsLoader', $defaultLoader);
         $this->assertTrue($defaultLoader === JsonLD::getRemoteDocumentLoader(), 'signleton');
 
+        // create mocked loader
         $mockedLoader = $this->createMockLoader();
         $this->assertInstanceOf('ML\\JsonLD\\RemoteDocumentLoader', $mockedLoader);
         $this->assertInstanceOf('ML\\JsonLD\\RemoteDocument', $mockedLoader->loadDocument('http://example.org/'));
 
+        // inject loader
         JsonLD::setRemoteDocumentLoader($mockedLoader);
         $this->assertTrue($mockedLoader === JsonLD::getRemoteDocumentLoader(), 'injection');
+
+        // restore loader
+        JsonLD::setRemoteDocumentLoader($defaultLoader);
     }
 
     /**
